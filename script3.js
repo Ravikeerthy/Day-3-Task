e16c2731248565eceaf0c03fe2f99bd0e3036e32
Ravikeerthy <98576564+Ravikeerthy@users.noreply.github.com>
2024-02-26 16:23:42 +0400

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,16 +1,18 @@
-let a = {
-    name :"Person 1",
-    age : 5
-}
+let obj1 = {name:"person1",age:5}
+let obj2 = {age:5, name:"person1"}
 
-let b = {
-    age:5,
-    name: "Person 1"
 
-}
+let obj11 = sorting(obj1);
+let obj22 = sorting(obj2);
 
-if (a==b){
-    console.log("Both are same");
+if(obj11 === obj22){
+    console.log("It is Equal");
 }else{
-    console.log("It is not same");
+    console.log("It is Not Equal");
+}
+
+function sorting(obj){
+   
+    return JSON.stringify(obj, Object.keys(obj).sort());
+   
 }
